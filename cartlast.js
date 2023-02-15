181508d4bf7b63ed461ead3c485d1ce978d2a594
Alexey <84179953+Aleksey-Alexandrovich@users.noreply.github.com>
2023-02-15 14:10:42 +0300

    Добавил файл ESLINT
    Исправил все ошибки при помощи ESLINT

--- cartlast.js
+++ cartlast.js
@@ -1,44 +1,42 @@
+'use strict';
 const cart = {
-	items: [],
-	totalPrice: 0,
-	count: 0,
+  items: [],
+  totalPrice: 0,
+  count: 0,
 
-get totalPrice() {
+  get totalprice() {
     return this.calculateItemPrice();
-},
-
-	add(name,price,number = 1) {
-		const product = {name,price,number};
-		this.items.push(product);
-	
-		
-	},
-	increaseCount(numb) {
-		return this.count + numb;
-	},
-
-	calculateItemPrice() {
-    return this.items.reduce((acc, {price, number}) =>
-      acc + price * number, 0);
-		
   },
 
-	clear() {
-		this.items = [];
+  add(name, price, number = 1) {
+    const product = {name, price, number};
+    this.items.push(product);
+  },
+  increaseCount(numb) {
+    return this.count + numb;
+  },
+
+  calculateItemPrice() {
+    return this.items.reduce((acc, {price, number}) =>
+      acc + price * number, 0);
+  },
+
+  clear() {
+    this.items = [];
     this.count = 0;
     this.discount = 0;
-		
-	},
-	print() {
-		console.log(`${JSON.stringify(this.items) }`)
-		console.log(`Общая сумма: ${this.totalPrice}`)
-	},
+  },
+  print() {
+    console.log(`${JSON.stringify(this.items)}`);
+    console.log(`Общая сумма: ${this.totalPrice}`);
+  },
 };
 
-const keys = Object.keys(cart)
+const keys = Object.keys(cart);
 console.log(keys);
 cart.add('Телевизор', 15600, 2);
 cart.add('Холодильник', 100000, 5);
 // cart.clear()//
-
 cart.print();
+
+
